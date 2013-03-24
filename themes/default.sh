5199c08f21117e01d570b3c77689cebf85942e0f
STAR_ZERO <kenji.01.star@gmail.com>
2013-03-24 18:03:01 +0900

    Change status

--- themes/default.sh
+++ themes/default.sh
@@ -24,9 +24,13 @@ TMUX_POWERLINE_DEFAULT_RIGHTSIDE_SEPARATOR=${TMUX_POWERLINE_DEFAULT_RIGHTSIDE_SE
 if [ -z $TMUX_POWERLINE_LEFT_STATUS_SEGMENTS ]; then
 	TMUX_POWERLINE_LEFT_STATUS_SEGMENTS=(
 		"tmux_session_info 148 234" \
-		"lan_ip 24 255 ${TMUX_POWERLINE_SEPARATOR_RIGHT_THIN}" \
-		"wan_ip 24 255" \
 		"hostname 33 0" \
+		"pwd 89 211" \
+		"vcs_branch 29 88" \
+		"vcs_compare 60 255" \
+		"vcs_staged 64 255" \
+		"vcs_modified 9 255" \
+		"vcs_others 245 0" \
 	)
 fi
 
